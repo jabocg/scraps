c4340211c0a31a0e1aed8ba1a6116df2ed485f85
Jacob Gersztyn <jabocg@gmail.com>
2017-06-07 14:30:25 -0700

    Update pathlib-file-copy.py

--- python/pathlib-file-copy.py
+++ python/pathlib-file-copy.py
@@ -1,8 +1,8 @@
 import pathlib
 
-source = Path(...)
-dest = Path(...)
-
-with source.open() as i:
-  with dest.open(mode='w') as o:
-    o.write(i.read())
+# source: pathlib.Path - source file to get contents of
+# dest: pathlib.Path - destination file to put contents in
+def copyFile(source, dest):
+  with source.open() as i:
+    with dest.open(mode='w') as o:
+      o.write(i.read())
