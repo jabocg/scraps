7e21a0df691d365102da313f8ca58b0a8b276afb
Jacob Gersztyn <jabocg@gmail.com>
2017-06-07 14:54:38 -0700

    Update pathlib-recursive-delete-directory.py

--- python/pathlib-recursive-delete-directory.py
+++ python/pathlib-recursive-delete-directory.py
@@ -2,7 +2,7 @@ import pathlib
 
 # path: pathlib.Path - directory to remove
 def deleteDirectory(path):
-  # NOTE: can replace five lines below with `removeConents(path)` scrap form pathlib-recursive-remove-contents
+  # NOTE: can replace five lines below with `deleteConents(path)` scrap form pathlib-recursive-remove-contents
   for i in path.glob('*'):
     if i.is_dir():
        deleteDirectory(i)
