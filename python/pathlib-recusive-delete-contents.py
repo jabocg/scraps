0e68675d50f8e6a9e649a44a442cbd7346864eec
Jacob Gersztyn <jabocg@gmail.com>
2017-06-07 14:31:32 -0700

    Update pathlib-recusive-delete-contents.py

--- python/pathlib-recusive-delete-contents.py
+++ python/pathlib-recusive-delete-contents.py
@@ -2,10 +2,10 @@ import pathlib
 
 # path: pathlib.Path - directory to remove contents of
 def removeContents(path):
-    for i in path.glob('*'):
-        if i.is_dir():
-            # NOTE: can replace both lines with `removeDirectory(i)` from pathlib-recusive-rmdir scrap
-            removeContents(i)
-            i.rmdir()
-        else:
-            i.unlink()
+  for i in path.glob('*'):
+    if i.is_dir():
+      # NOTE: can replace both lines with `removeDirectory(i)` from pathlib-recusive-rmdir scrap
+      removeContents(i)
+      i.rmdir()
+    else:
+      i.unlink()
