e8910ae832fea3f09844e8fea66e5e7b79fc3a92
Jacob Gersztyn <jabocg@gmail.com>
2017-06-07 14:38:36 -0700

    Update and rename pathlib-recursive-rmdir.py to pathlib-recursive-delete-directory.py

---
+++ python/pathlib-recursive-delete-directory.py
@@ -0,0 +1,11 @@
+import pathlib
+
+# path: pathlib.Path - directory to remove
+def deleteDirectory(path):
+  # NOTE: can replace five lines below with `removeConents(path)` scrap form pathlib-recursive-remove-contents
+  for i in path.glob('*'):
+    if i.is_dir():
+       deleteDirectory(i)
+    else:
+      i.unlink()
+  path.rmdir()
